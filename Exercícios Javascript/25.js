a4e0fce6503fa193069901a616053378016a028f
unknown <brunoferreirasmv@gmail.com>
2021-01-04 10:43:19 -0300

    ...

--- Exercícios Javascript/25.js
+++ Exercícios Javascript/25.js
@@ -3,9 +3,29 @@ A fim de criar um mecanismo de busca para sua aplicação, você precisa iniciar
 identificar palavras semelhantes.
 Escreva uma função que recebe como primeiro parâmetro uma palavra e, como segundo parâmetro, um array
 de strings. A função deverá filtrar as palavras do array que contêm nelas a string do primeiro parâmetro.
+
+buscarPalavrasSemelhantes("pro", ["programação", "mobile", "profissional"]) // retornará ["programação", "profissional"]
+buscarPalavrasSemelhantes("python", ["javascript", "java", "c++"]) // retornará []
 */
 
-function 
+function buscarPalavrasSemelhantes(string, arrayDeString){
+    let b,c, a = [];
+    b = 0;
+    for(i = 0; i < arrayDeString.length; i++){
+        let z=0;
+        do{
+            let c = 0;
+            for(j = b; j < arrayDeString.length; j++){
+                if(string[c] == arrayDeString[j]){
+                    a[i] = a[i] + 1;
+                }
+                c++;
+            }
+            b++;
+        }while(a[i]!=3 || j < arrayDeString.length)
+        console.log(a);
+    }
+    
+}
 
-console.log(buscarPalavrasSemelhantes("pro", ["programação", "mobile", "profissional"]));
-console.log(buscarPalavrasSemelhantes("python", ["javascript", "java", "c++"]));
+buscarPalavrasSemelhantes("pro", ["programação", "mobile", "profissional"])
