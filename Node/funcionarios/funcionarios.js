7e6b836f68f45a9710f45a4a5d5723a00070ebf6
unknown <brunoferreirasmv@gmail.com>
2021-01-14 15:00:48 -0400

    ...

--- Node/funcionarios/funcionarios.js
+++ Node/funcionarios/funcionarios.js
@@ -1,7 +1,20 @@
     const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
     const axios = require('axios')//axios é um cliente http, ele faz requisições para obter informaões 
                                   // de algo que esta remoto
+
+const chineses = f => f.pais === 'China'
+const mulheres = f => f.genero === 'F'
+const menorSalario = (func, funcAtual) =>{
+    return func.salario < funcAtual.salario ?func:funcAtual
+}
 axios.get(url).then(response => {
     const funcionarios = response.data
     console.log(funcionarios);
+
+//mulheres chinesas com menor salario?
+const func = funcionarios
+    .filter(chineses)
+    .filter(mulheres)
+    .reduce(menorSalario)
+console.log(func);
 })
